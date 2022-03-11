00a9d6db8812480e0f225e3bf2c4eb28a6fd8bf6
Fil Karnicki <filip.karnicki@gmail.com>
2022-03-11 14:12:07 +0100

    [FLINK-26570][statefun] Remote module configuration interpolation

--- statefun-flink/statefun-flink-core/src/main/java/org/apache/flink/statefun/flink/core/jsonmodule/RemoteModule.java
+++ statefun-flink/statefun-flink-core/src/main/java/org/apache/flink/statefun/flink/core/jsonmodule/RemoteModule.java
@@ -23,16 +23,22 @@ import static org.apache.flink.statefun.flink.core.spi.ExtensionResolverAccessor
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.ArrayNode;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.TextNode;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.ValueNode;
 import org.apache.flink.statefun.extensions.ComponentBinder;
 import org.apache.flink.statefun.extensions.ComponentJsonObject;
 import org.apache.flink.statefun.flink.core.spi.ExtensionResolver;
 import org.apache.flink.statefun.sdk.spi.StatefulFunctionModule;
 
 public final class RemoteModule implements StatefulFunctionModule {
-
+  private static final Pattern replaceRegex = Pattern.compile(".*(\\$\\{?([^}]+)}).*");
   private final List<JsonNode> componentNodes;
 
   RemoteModule(List<JsonNode> componentNodes) {
@@ -42,7 +48,7 @@ public final class RemoteModule implements StatefulFunctionModule {
   @Override
   public void configure(Map<String, String> globalConfiguration, Binder moduleBinder) {
     parseComponentNodes(componentNodes)
-        .forEach(component -> bindComponent(component, moduleBinder));
+        .forEach(component -> bindComponent(component, moduleBinder, globalConfiguration));
   }
 
   private static List<ComponentJsonObject> parseComponentNodes(
@@ -53,10 +59,64 @@ public final class RemoteModule implements StatefulFunctionModule {
         .collect(Collectors.toList());
   }
 
-  private static void bindComponent(ComponentJsonObject component, Binder moduleBinder) {
+  private static void bindComponent(
+      ComponentJsonObject component, Binder moduleBinder, Map<String, String> configuration) {
+    resolvePlaceholders(component.specJsonNode(), configuration);
     final ExtensionResolver extensionResolver = getExtensionResolver(moduleBinder);
     final ComponentBinder componentBinder =
         extensionResolver.resolveExtension(component.binderTypename(), ComponentBinder.class);
     componentBinder.bind(component, moduleBinder);
+  }
+
+  private static void resolvePlaceholders(
+      JsonNode specJsonNode, Map<String, String> configuration) {
+    resolvePlaceholders(null, null, specJsonNode, null, configuration);
+  }
+
+  private static void resolvePlaceholders(
+      JsonNode parent,
+      String nodeName,
+      JsonNode node,
+      Integer nodePositionInParentArray,
+      Map<String, String> config) {
+    if (node.isValueNode()) {
+      resolveValueNode(parent, nodeName, (ValueNode) node, nodePositionInParentArray, config);
+    } else if (node.isArray()) {
+      resolveArray((ArrayNode) node, config);
+    } else if (node.isObject()) {
+      resolveObject((ObjectNode) node, config);
+    }
+  }
+
+  private static void resolveValueNode(
+      JsonNode parent,
+      String nodeName,
+      ValueNode node,
+      Integer nodePositionInParentArray,
+      Map<String, String> config) {
+    Matcher m = replaceRegex.matcher(node.textValue());
+    if (m.matches() && config.containsKey(m.group(2))) {
+      if (parent.isObject()) {
+        ((ObjectNode) parent)
+            .put(nodeName, node.textValue().replace(m.group(1), config.get(m.group(2))));
+      } else if (parent.isArray()) {
+        ArrayNode anParent = ((ArrayNode) parent);
+        anParent.set(
+            nodePositionInParentArray,
+            new TextNode(node.textValue().replace(m.group(1), config.get(m.group(2)))));
+      }
+    }
+  }
+
+  private static void resolveObject(ObjectNode node, Map<String, String> config) {
+    node.fields()
+        .forEachRemaining(
+            kvp -> resolvePlaceholders(node, kvp.getKey(), kvp.getValue(), null, config));
+  }
+
+  private static void resolveArray(ArrayNode node, Map<String, String> config) {
+    for (int i = 0; i < node.size(); i++) {
+      resolvePlaceholders(node, null, node.get(i), i, config);
+    }
   }
 }
